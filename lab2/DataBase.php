908253b18104efda97e4b9bfe7b8f5800c9ec128
Kortald <90599529+Kortald@users.noreply.github.com>
2022-02-07 03:01:31 +0300

    Add files via upload

---
+++ lab2/DataBase.php
@@ -0,0 +1,66 @@
+<?php
+$db_driver='mysql';
+$db_user='root';
+$db_pass='root';
+$db_name='medci';
+$db_server='localhost';
+//создали pdo подключение
+$connect="$db_driver:host=$db_server;dbname=$db_name";//создали подключение к нашей базе
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo=new PDO($connect, $db_user, $db_pass, $opt);
+$doctors=array();
+$sicklists=array();
+$sqlname="SELECT name FROM sicklists";
+$query=$pdo->query($sqlname);
+while($tmp=$query->fetch())
+{
+    $sicklists[]=$tmp;
+}
+$sqlfio="SELECT fio FROM doctors";
+$query=$pdo->query($sqlfio);
+while($tmp=$query->fetch())
+{
+    $doctors[]=$tmp;
+}
+$rez=array();
+$array_zapr=array();
+
+$fl=0;
+foreach($_POST as $item) {//проверяем пуст ли массив
+    if ($item!='') $fl=1;
+}
+if($fl==1) {//если не пуст
+
+    $sql="SELECT * FROM sicklists INNER JOIN doctors ON DocID=ID WHERE id>0";
+    if($_POST['name']) {
+        $sql.=" AND name=? ";
+        $array_zapr[]=$_POST['name'];
+    }
+    if($_POST['fio']) {
+        $sql.=" AND doctors.fio=? ";
+        $array_zapr[]=$_POST['fio'];
+    }
+
+    $query=$pdo->prepare($sql);
+    $query->execute($array_zapr);
+    while($tmp=$query->fetch())
+    {
+        $rez[]=$tmp;
+    }
+}
+else//если пуст
+{
+    // $sql="SELECT tab.* , teacherid.id, teacherid.fio  FROM tableica tab INNER JOIN teacherid  ON tab.teacher=teacherid.id  ";
+    $sql="SELECT * FROM sicklists INNER JOIN doctors ON DocID=ID";
+    $query=$pdo->query($sql);
+    while($tmp=$query->fetch())
+    {
+        $rez[]=$tmp;
+    }
+}
+?>
+
---
+++ lab2/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</head>
+<body class="containter-fluid">
+<head>
+    <?php
+    include("MEDsiCenter.php");
+    include("DataBase.php");
+    ?>
+</head>
+
+<div class="filtr">
+    <?php if(count($rez)>0):?>
+<table class="table" style="margin-top:2em">
+    <thead>
+    <tr>
+        <th scope="col">Скан листа</th>
+        <th scope="col">Имя пациента</th>
+        <th scope="col">Лечащий врач</th>
+        <th scope="col">Диагноз</th>
+        <th scope="col">Продолжительность болезни(в днях)</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <?php foreach($rez as $item){?>
+    <tr>
+        <th scope="row"><img src="inc/Tab_imgs/<?=$item['ListScan']?>" style="height: 150px;width: 150px;"></th>
+        <td><?=$item['name']?></td>
+        <td><?=$item['fio']?></td>
+        <td><?=$item['Diagnosis']?></td>
+        <td><?=$item['IllnessDuration']?></td>
+    </tr>
+    <?php };?>
+
+    </tbody>
+    </table>
+    <?php endif;?>
+</div>
+</body>
+</html>
